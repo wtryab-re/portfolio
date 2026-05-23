50a4a5f42e940b84a32caec248bd3cfbfce23983
wtryab <wtryab@gmail.com>
2026-05-23 14:42:50 +0500

    about section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -32,7 +32,7 @@ export default function Contact() {
         <h2 className="text-4xl font-bold mb-8 text-slate-800">contact me</h2>
       </div>
       <div>
-        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4" netlify>
           {/* Essential hidden field for React bot crawlers */}
           <input type="hidden" name="form-name" value="contact" />
 
